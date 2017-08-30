c7c5f0852e0877d81d07f86741546fcff7cd6ef8
cash <cash@cashwu.com>
2017-08-30 18:19:20 +0800

    init

--- Kata20170830_SumStringsasNumbers/SumStringsasNumbersTests.cs
+++ Kata20170830_SumStringsasNumbers/SumStringsasNumbersTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170830_SumStringsasNumbers
 {
     [TestClass]
-    public class UnitTest1
+    public class SumStringsasNumbersTests
     {
         [TestMethod]
         public void TestMethod1()
