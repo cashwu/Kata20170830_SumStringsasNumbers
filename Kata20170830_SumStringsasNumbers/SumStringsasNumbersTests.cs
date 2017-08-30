f5e8911276f0ee78d9af7c8ae2695e98a84d6565
cash <cash@cashwu.com>
2017-08-30 18:23:40 +0800

    input 1, 1 test case

--- Kata20170830_SumStringsasNumbers/SumStringsasNumbersTests.cs
+++ Kata20170830_SumStringsasNumbers/SumStringsasNumbersTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170830_SumStringsasNumbers
     public class SumStringsasNumbersTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_and_1_should_be_2()
         {
+            sumStringsShouldBe("2", "1", "1");
+        }
+
+        private static void sumStringsShouldBe(string expected, string a, string b)
+        {
+            var kata = new Kata();
+            var actual = kata.sumStrings(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string sumStrings(string a, string b)
+        {
+            return "2";
         }
     }
 }
